03b06e003c8e5466fc76963213d18199eff7f880
Drewthurm21 <drewthurm21@gmail.com>
2021-05-05 10:34:34 -0700

    added route to return all events

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -9,11 +9,13 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/')
 @login_required
 def users():
-    users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
-
+    all_users = User.query.all()
+    users = [user.to_dict() for user in users]
+    return jsonify(users)
 
 # return single user
+
+
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
