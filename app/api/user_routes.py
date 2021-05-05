8ae0413eb7f0745f93f19257c59eb751ffc2a007
Drewthurm21 <drewthurm21@gmail.com>
2021-05-05 10:06:08 -0700

    added route to get registrations by user id

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User, Event
+from app.models import User, Event, Registration
 
 user_routes = Blueprint('users', __name__)
 
@@ -9,7 +9,7 @@ user_routes = Blueprint('users', __name__)
 @login_required
 def users():
     users = User.query.all()
-    return {"users": [user.to_dict() for user in users]}
+    return {'users': [user.to_dict() for user in users]}
 
 
 @user_routes.route('/<int:id>')
@@ -25,3 +25,10 @@ def user_events(id):
     user_events = Event.query.filter_by(host_id=id).all()
     events = [event.to_dict() for event in user_events]
     return jsonify(events)
+
+
+@user_routes.route('/<int:id>/registrations')
+@login_required
+def user_registrations(id):
+    user_registrations = Registration.query.filter_by(user_id=id).all()
+    return jsonify([registration.to_dict() for registration in user_registrations])
