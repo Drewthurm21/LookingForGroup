176d3888fe0440e31023ad9e8436a25bfd145ca7
Drewthurm21 <drewthurm21@gmail.com>
2021-05-05 10:20:20 -0700

    added route to get comments posted by a user by id

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User, Event, Registration
+from app.models import User, Event, Registration, Comment
 
 user_routes = Blueprint('users', __name__)
 
 
+# return all users
 @user_routes.route('/')
 @login_required
 def users():
@@ -12,6 +13,7 @@ def users():
     return {'users': [user.to_dict() for user in users]}
 
 
+# return single user
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
@@ -19,6 +21,7 @@ def user(id):
     return user.to_dict()
 
 
+# return events hosted by a user
 @user_routes.route('/<int:id>/events')
 @login_required
 def user_events(id):
@@ -27,8 +30,30 @@ def user_events(id):
     return jsonify(events)
 
 
+# return registrations for a user
 @user_routes.route('/<int:id>/registrations')
 @login_required
 def user_registrations(id):
     user_registrations = Registration.query.filter_by(user_id=id).all()
-    return jsonify([registration.to_dict() for registration in user_registrations])
+    registrations = [registration.to_dict()
+                     for registration in user_registrations]
+    return jsonify(registrations)
+
+
+# return comments posted by a user
+@user_routes.route('/<int:id>/comments')
+@login_required
+def user_comments(id):
+    user_comments = Comment.query.filter_by(user_id=id).all()
+    comments = [comment.to_dict() for comment in user_comments]
+    return jsonify(comments)
+
+
+# delete a user
+@user_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return user.to_dict()
