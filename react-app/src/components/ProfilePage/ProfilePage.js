892a07898314b5ea5af64e33d5ee690f4a67358a
Drewthurm21 <drewthurm21@gmail.com>
2021-05-27 18:30:51 -0700

    made profile page more responsive.

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -24,31 +24,40 @@ function ProfilePage() {
 
   return user && user_events && (
     <div className='profile-page-wrapper'>
-      <img className='profile-avatar photo' src={user.avatars} alt=''></img>
-      <div className='profile-sidebar stats'>
-        <div className='sidebar-wrapper sidebar'>
-          <h1>Welcome {user.username}</h1>
-          <br></br>
-          <h3></h3>
-          <br></br>
-          <br></br>
+      <div className='topp'>
+        <img className='profile-avatar photo' src={user.avatars} alt=''></img>
+        <div className='profile-sidebar stats'>
+          <div className='sidebar-wrapper sidebar'>
+            <h1>Welcome {user.username}</h1>
+            <br></br>
+            <h2>It's currently</h2>
+            <h3>{String(new Date())}</h3>
+            <br></br>
+            <div className='post-event-btn' onClick={startEventPost}>Post an Event!</div>
+            <br></br>
 
-          <div className='post-event-btn' onClick={startEventPost}> POST EVENT</div>
+          </div>
         </div>
       </div>
-      <div className='user-events attend'>
-        <div className='hosting'>
-          <h2>You're registered for {user_events.registeredEvents.length} events!</h2>
+      <div className='bott'>
+        <div className='user-events attend'>
+          <div className='hosting'>
+            <h2>You're registered for {user_events.registeredEvents.length} events!</h2>
+          </div>
+          <br></br>
+          <div className='event-cards'>
+            {user_events.registeredEvents.map(event => <EventCardLong event={event} key={event.id} />)}
+          </div>
         </div>
-        <br></br>
-        {user_events.registeredEvents.map(event => <EventCardLong event={event} key={event.id} />)}
-      </div>
-      <div className='user-events host'>
-        <div className='hosting'>
-          <h2>{`You're hosting ${user_events.hostedEvents.length} upcoming events`}</h2>
+        <div className='user-events host'>
+          <div className='hosting'>
+            <h2>You're hosting {user_events.hostedEvents.length} upcoming events!</h2>
+          </div>
+          <br></br>
+          <div className='event-cards'>
+            {user_events.hostedEvents.map(event => <EventCardLong event={event} key={event.id} />)}
+          </div>
         </div>
-        <br></br>
-        {user_events.hostedEvents.map(event => <EventCardLong event={event} key={event.id} />)}
       </div>
     </div>
   );
